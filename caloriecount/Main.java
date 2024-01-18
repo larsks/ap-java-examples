7673bdbf57ec142e80b46070075298438ecd1b31
Lars Kellogg-Stedman <lars@oddbit.com>
2024-01-18 17:37:35 -0500

    Add driver class for caloriecount

---
+++ caloriecount/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		CalorieCount sunday = new CalorieCount(1500);
+		sunday.addMeal(716, 38, 38, 45);
+		sunday.addMeal(230, 16, 8, 16);
+		sunday.addMeal(568, 38, 50, 24);
+
+		assert !sunday.onTrack();
+		assert Math.abs(0.24 - sunday.getProteinPercentage()) < 0.005;
+	}
+}
