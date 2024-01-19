1aea03c65f603e256dcdb238d40df221185267b4
Lars Kellogg-Stedman <lars@oddbit.com>
2024-01-19 17:16:28 -0500

    Add Makefile for caloriecount

--- caloriecount/Main.java
+++ caloriecount/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
 		CalorieCount sunday = new CalorieCount(1500);
+		assert sunday.onTrack();
+
 		sunday.addMeal(716, 38, 38, 45);
 		sunday.addMeal(230, 16, 8, 16);
 		sunday.addMeal(568, 38, 50, 24);
